2b48c05f69abd881dfe16e95d6f331a144e8504e
suzietstephen32 <suziestephen@192-168-1-100.tpgi.com.au>
2021-02-08 20:09:49 +1100

    main files edited

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,106 @@
-// TODO: Include packages needed for this application
+// Required inquirer, fs, generateMarkdown
+const inquirer          = require("inquirer");
+const fs                = require("fs");
+const generateMarkdown  = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-function init() {}
+// User data question for inquirer to ask
+const gitHubQuestion = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is your full name?"
+    },{
+        type: "input",
+        name: "username",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Your email:',
+    },
+    {
+        type: 'input',
+        name: 'deployedLink',
+        message: 'Link to your deployed project:',
 
-// Function call to initialize app
-init();
+    },
+];
+
+
+// Project questions for inquirer to ask
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of your project?"
+    },
+    {
+        type: "input",
+        name: "badge",
+        message:  "Please provide your badge links."
+    },
+    {
+        type: "input",
+        name: "description",
+        message:  "Please provide a description of your project."
+    },
+    {
+        type: "input",
+        name: "installation",
+        message:  "What do you need to install in order for your application to work?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message:  "How do you use your this program?"
+    },
+    {
+        type: 'input',
+        name: 'features',
+        message: "Describe the features of your project:",
+    },
+    {
+        type: "input",
+        name: "tests",
+        message:  "How would you run tests on this project?"
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose your license:',
+        choices: ['MIT', 'Apache 2.0', 'Creative Commons', 'GPLv2', 'GPLv3', 'no license'],
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "How do other developers to contribute to your project?",
+    },
+];
+
+
+
+        // Function to write readme file
+
+        function writeToFile(fileName, data) {
+            fs.writeFile(fileName, data, (err) =>
+                err ? console.error(err) : console.log('Success')
+                )
+        }
+
+        // Create a function to initialize app
+        function init() {
+            inquirer
+                .prompt (questions)
+                    .then((data) =>{
+                        const myReadme = generateMarkdown(data);
+                        writeToFile('README.md', myReadme);
+                    });
+        }
+
+        // Function call to initialize app
+        init();
+
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,53 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ${data.badge}
+
+
+
+  # Description
+  ${data.description}
+
+  [See deployed application.](${data.deployedLink})
+
+
+  # Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Features] (#features)
+  * [Tests](#tests)
+  * [License](#license)
+  * [Contributions](#contributions)
+  * [Questions](#questions)
+  
+
+  
+  # Installation
+  ${data.installation}
+  # Usage
+  ${data.usage}
+  # Features
+  ${data.features}
+  # Tests
+  ${data.tests}
+  # License
+  ${data.license}
+  # Contributions
+  ${data.contributors}
+
+
+  # Repository
+  ${data.repository}
+  # Deployed Link
+  ${data.githublink}
+  
+
+  # Questions
+  ${data.questions}
+  Got questions or feedback? You can contact me on:
+    - [Github](https://github.com/${data.username})
+    - [Email](mailto:${data.email})
 
 `;
 }
